a2fc0732ffd51c251fe209a63aaff5e4315ad371
ALI Raza <036630aliraza@gmail.com>
2026-03-29 22:25:19 +0500

    add Insert at begning and mid fuction is add and fix bugs

--- SigleLinkedList.py
+++ SigleLinkedList.py
@@ -2,31 +2,58 @@ class Node:
     def __init__(self, info, next = None):
         self.data = info
         self.next = next
-
-class SinglyLinkedList:
+        
+class SinglyLinkList:
     def __init__(self, head = None):
         self.head = head
 
-    def InsertAtEnd(self, value):
+    def InsertAtBeg(self, value):
         temp = Node(value)
-        
-        if(self.head != None):
+        temp.next = self.head
+        self.head = temp
+
+
+    def InsertAtMid(self, value, loc):
+        temp = Node(value)
+        t1 = self.head
+        if self.head is None:
+            self.head = temp
+            return    
+        while t1 is not None:
+            if t1.data == loc:
+                temp.next = t1.next
+                t1.next = temp
+                return
+            t1 = t1.next
+        print(f"{loc} not found")
+
+
+    def InsertAtEnd(self,value):
+        if self.head is not None:
+            temp = Node(value)
             t1 = self.head
-            while(t1.next != None):
+            while t1.next is not None:
                 t1 = t1.next
             t1.next = temp
         else:
             self.head = temp
-            
-    def PrintLL(self):
-        t1 = self.head
-        while (t1.next != None):
-            print(t1.data)
-            t1 = t1.next
-        print(t1.data)
         
-obj = SinglyLinkedList()
-obj.InsertAtEnd(5)
+    def printLL(self):
+        t1 = self.head
+        if t1 is None:
+            print("Empity list!")
+        while t1 is not None:
+                print(t1.data)
+                t1 = t1.next
+
+
+obj = SinglyLinkList()
+obj.InsertAtBeg(3)
+obj.InsertAtBeg(2)
+obj.InsertAtBeg(1)
 obj.InsertAtEnd(6)
+obj.InsertAtMid(2,2)
+obj.InsertAtMid(2,4)
 obj.InsertAtEnd(7)
-obj.PrintLL()
+
+obj.printLL()
